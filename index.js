962bac427471e839210d097438fb1ead1d5e64b8
Shirooo098 <johndavid8803@gmail.com>
2025-01-18 18:25:55 +0800

    Complete Geo To Latitude-Longitude

--- index.js
+++ index.js
@@ -5,27 +5,40 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 const GEOAPI_URL = "http://api.openweathermap.org/geo/1.0/zip?";
+const WEATHERAPI_URL = "https://api.openweathermap.org/data/2.5/weather?";
 const API_TOKEN = "2755ab8796f4ba56de06ee465fdfdebf";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended : true}));
 
 app.get("/",  async (req, res) => {
-    res.render("index.ejs", { content : "Waiting for Location..."});
+    res.render("index.ejs");
 });
 
 app.post("/get-location", async (req, res) => {
     try{
         const zipCode = req.body.zipCode;
         const iso = req.body.iso;
-        const result = await axios.get(GEOAPI_URL, {
+        const locResult = await axios.get(GEOAPI_URL, {
             params: {
                 zip: `${zipCode},${iso}`,
                 appid: API_TOKEN
             }
         });
-        console.log(result.data);
-        res.render("index.ejs", { data: result.data});
+
+        const {lat, lon} = locResult.data;
+
+        const weatherResult = await axios.get(WEATHERAPI_URL, {
+            params: {
+                lat: lat,
+                lon: lon,
+                appid: API_TOKEN,
+                units: "metric",
+            },
+        });
+
+        console.log(weatherResult.data);
+        res.render("index.ejs", { data: weatherResult.data});
     }catch(error){
         console.log(error.response.data);
     }
